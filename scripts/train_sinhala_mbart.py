e52b39075da6cec1c36b57c9eb9c5f71e4177024
Lahiru Udayanga <lahirudesilva.17@cse.mrt.ac.lk>
2021-07-22 00:03:14 +0530

    sinhala sample dataset added

--- scripts/train_sinhala_mbart.py
+++ scripts/train_sinhala_mbart.py
@@ -3,7 +3,7 @@ from muss.fairseq.main import fairseq_train_and_evaluate_with_parametrization
 from muss.mining.training import get_mbart_kwargs
 
 
-sin15M = 'filename'
+sin15M = 'sin15M'
 kwargs = get_mbart_kwargs(dataset=sin15M, language='si', use_access=True)
 kwargs['train_kwargs']['ngpus'] = 1  # Set this from 8 to 1 for local training
 kwargs['train_kwargs']['max_tokens'] = 512  # Lower this number to prevent OOM
