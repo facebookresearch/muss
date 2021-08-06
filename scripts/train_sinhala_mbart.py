f887860371302148a670580c3bd866f3fa25fcc6
Lahiru Udayanga <lahirudesilva.17@cse.mrt.ac.lk>
2021-08-07 03:54:04 +0530

    test params

--- scripts/train_sinhala_mbart.py
+++ scripts/train_sinhala_mbart.py
@@ -6,5 +6,5 @@ from muss.mining.training import get_mbart_kwargs
 newsela = 'newsela'
 kwargs = get_mbart_kwargs(dataset=newsela, language='si', use_access=False)
 kwargs['train_kwargs']['ngpus'] = 1  # Set this from 8 to 1 for local training
-kwargs['train_kwargs']['max_tokens'] = 1024  # Lower this number to prevent OOM
+kwargs['train_kwargs']['max_tokens'] = 32  # Lower this number to prevent OOM
 result = fairseq_train_and_evaluate_with_parametrization(**kwargs)
