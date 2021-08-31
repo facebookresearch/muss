ca7fc717eff2d14b5f0a8ecc360721bf8d669c4b
Lahiru Udayanga <lahirudesilva.17@cse.mrt.ac.lk>
2021-08-31 12:31:40 +0530

    file open in w+

--- scripts/simplify.py
+++ scripts/simplify.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     source_sentences = read_lines(args.filepath)
     pred_sentences = simplify_sentences(source_sentences, model_name=args.model_name)
-    outfile = open("si_simplified.txt")
+    outfile = open("si_simplified.txt","w+")
     for c, s in zip(source_sentences, pred_sentences):
         print('-' * 80)
         print(f'Original:   {c}')
