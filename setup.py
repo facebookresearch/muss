f1b511c3c419d6826bb6817f5647f44ae0ed1c45
Louis Martin <louismartincs@gmail.com>
2021-10-08 11:20:34 +0100

    Take comments into account

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
-with open('requirements.txt', 'r', encoding='utf-8') as f:
+with open('requirements.txt', 'r') as f:
     requirements = f.read().strip().split('\n')
 
 setup(
