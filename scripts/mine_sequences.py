d52335ceb3fddd0e19c95bb03fb91034c854d393
Raphael Assis <58819530+AssisRaphael@users.noreply.github.com>
2022-09-20 20:15:38 -0300

    feat: remove job atribute from executor batch context

--- scripts/mine_sequences.py
+++ scripts/mine_sequences.py
@@ -133,10 +133,7 @@ with log_action('Computing embeddings'):
             if get_index_path(sentences_path, indexes_dir).exists():
                 continue
             # Should take about 30 minutes each
-            job = executor.submit(
-                compute_and_save_embeddings, sentences_path, base_index_path, get_embeddings, indexes_dir=indexes_dir
-            )
-            job.result()
+            compute_and_save_embeddings(sentences_path, base_index_path, get_embeddings, indexes_dir=indexes_dir)
 
 # Mine the paraphrases
 with log_action('Mining paraphrases'):
@@ -156,8 +153,7 @@ with log_action('Mining paraphrases'):
             if get_results_path(query_sentences_path, db_sentences_paths, topk, nprobe, nn_search_results_dir).exists():
                 continue
             # Should take about ~1h30 each
-            job = executor.submit(
-                compute_and_save_nn_batched,
+            compute_and_save_nn_batched(
                 query_sentences_path,
                 db_sentences_paths,
                 topk,
@@ -166,7 +162,6 @@ with log_action('Mining paraphrases'):
                 nn_search_results_dir,
                 delete_intermediary=True,
             )
-            job.result()
 
 # Filter candidate paraphrases
 with log_action('Filtering candidate paraphrases'):
